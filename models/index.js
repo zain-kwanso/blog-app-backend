ab1119fbf7b8e5665401014aa4a0956977c58622
zain-kwanso <zain.tahir@kwanso.com>
2024-07-24 12:26:27 +0500

    changes for vercel deployement

--- models/index.js
+++ models/index.js
@@ -10,15 +10,16 @@ const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (env == "production") {
+  sequelize = new Sequelize(process.env.POSTGRES_URL, {
+    dialect: config.dialect,
+    dialectModule: require("pg"),
+  });
 } else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: config.dialect,
+  });
 }
 
 fs.readdirSync(__dirname)
